88460ad7bb1d74695e8753354b9bd1d788e4df7c
Tahseen <>
2022-02-03 10:13:34 +1000

    more optimised solutions

--- 1 - Two Sum.js
+++ 1 - Two Sum.js
@@ -1,17 +1,35 @@
-nums = [3, 2, 4];
-target = 6;
+const nums = [3, 2, 4];
+const target = 6;
+
+//Bruteforce
+// function sumFunction(nums, target) {
+//   let index = [];
+//   for (let i = 0; i < nums.length; ++i) {
+//     for (let j = i + 1; j < nums.length; ++j) {
+//       if (nums[i] + nums[j] === target) {
+//         index.push(i);
+//         index.push(j);
+//       }
+//     }
+//   }
+//   return index;
+// }
+
+// console.log(sumFunction(nums, target));
+
+// optimized method
+const nums = [3, 2, 4];
+const target = 6;
 
 function sumFunction(nums, target) {
-  let index = [];
-  for (let i = 0; i < nums.length; ++i) {
-    for (let j = i + 1; j < nums.length; ++j) {
-      if (nums[i] + nums[j] === target) {
-        index.push(i);
-        index.push(j);
-      }
+  let diff;
+  for (let i = 0; i < nums.length; i++) {
+    diff = target - nums[i];
+    diffIndex = nums.indexOf(diff);
+    if (diffIndex !== -1 && diffIndex !== i) {
+      return [i, diffIndex];
     }
   }
-  return index;
 }
 
 console.log(sumFunction(nums, target));
--- 121 - Best Time to Buy and Sell Stock.js
+++ 121 - Best Time to Buy and Sell Stock.js
@@ -13,7 +13,7 @@ prices = [7, 1, 5, 3, 6, 4];
 //   return profit;
 // }
 
-// optimised solution 149ms 32.3% faster, 51.3MB
+// optimised solution
 function maxProfit(prices) {
   profit = 0;
   let minVal = Infinity;
